e8daef1b6858f67e3d01dd8c3a4a7adc5e3e2083
Luis Arrais <lfbarrais@gmail.com>
2018-04-15 16:34:28 -0300

    Adiciona controller inicial e respostas para time e help

---
+++ src/index.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const Telegram = require('telegram-node-bot');
+const TelegramBaseController = Telegram.TelegramBaseController;
+const TextCommand = Telegram.TextCommand;
+const chatbot = new Telegram.Telegram('596233039:AAEXvTJMo-8XVRNXJEoH8st9KgSX2S2x8cw');
+const utils = require('./util')
+
+class EventsController extends TelegramBaseController {
+  tempoAction(scope) {
+    let msg = utils.buildStringTimeRemaning();
+    scope.sendMessage(msg, { parse_mode: 'HTML' });
+  }
+  
+  helpAction(scope) {
+    let msg = 'Este bot conta o tempo restante para ir para casa. \nUse o comando /tempo para ver o tempo restante.'
+    scope.sendMessage(msg, { parse_mode: 'HTML' });
+  }
+
+  get routes() {
+    return {
+      'tempo': 'tempoAction',
+      'help': 'helpAction',
+    }
+  }
+}
+
+chatbot.router
+  .when(new TextCommand('/tempo', 'tempo'), new EventsController())
+  .when(new TextCommand('/help', 'help'), new EventsController())
+  
+
